6a4a2ef96bf3e6981ae153cd31bed32b0ff8caa6
Ancor Gonzalez Sosa <ancor@suse.de>
2013-10-10 17:05:09 +0200

    Removing outdated comments

--- app/controllers/devise/ichain_sessions_controller.rb
+++ app/controllers/devise/ichain_sessions_controller.rb
@@ -1,19 +1,16 @@
 class Devise::IchainSessionsController < DeviseController
   prepend_before_filter :require_no_authentication, :only => [ :new, :test ]
 
-  # GET /resource/sign_in
   def new
     return new_test if ::Devise.ichain_test_mode
     self.resource = resource_class.new
     @back_url = base_url + after_sign_in_path_for(resource_name)
-    # The slash at the end is very important
     @login_url = resource_class.ichain_login_url
     @context = ::Devise.ichain_context
     @proxypath = ::Devise.ichain_proxypath
     respond_with resource
   end
 
-  # DELETE /resource/sign_out
   def destroy
     redirect_url = base_url + after_sign_out_path_for(resource_name)
     if ::Devise.ichain_test_mode
@@ -23,7 +20,6 @@ class Devise::IchainSessionsController < DeviseController
       set_flash_message :notice, :signed_out if signed_out && is_navigational_format?
       redirect_to redirect_url
     else
-      # The final slash is very important, indeed
       logout_url = resource_class.ichain_logout_url
       logout_url += "?" + {:url => redirect_url}.to_query
       redirect_to logout_url
